9b0c597f061484fbdf2b1275d4e0824da383c990
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-04-27 16:33:01 -0300

    exe 008

---
+++ aula11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 70;
+console.log('A velocidade do seu carro é ' + vel + ' Km/h');
+if(vel > 60){ //condição simples
+    console.log('Voce ultrapassou a velocidade permitida!');
+}
+console.log('Dirija com segurança!')
