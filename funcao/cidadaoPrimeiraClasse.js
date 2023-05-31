489a9ca36e11b23cd738d8248f451cc771cf9bfa
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-05-30 22:59:42 -0300

    function

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,39 @@
+//Função em JS é First-Class Object (Citizens)
+//Higher-order function
+
+//criar de fomar literal
+function fun1(){
+
+}
+
+//Armazenar em uma variável
+const fun2 = function(){
+
+}
+
+//Armazenar em um array
+const array = [function (a, b){ return a + b }, fun1, fun2]
+console.log(array[0](2, 3))
+
+//Armazenar em atributo de objeto
+const obj = {}
+obj.falar = function(){return 'Olá'}
+console.log(obj.falar())
+
+//Passar função como paramentro
+function run(fun){
+    fun()
+}
+run(function() { console.log('Executando') })
+
+
+//Uma função pode retornar/conter uma função
+function soma (a, b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
+const cincoMais = soma(2, 3)
+cincoMais(4)
