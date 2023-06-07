d1177880625cb6d7141ca3c4abe9de0e4141f91d
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-06-06 22:24:22 -0300

    call back 2

--- funcao/callback2.js
+++ funcao/callback2.js
@@ -1,9 +1,22 @@
 const notas = ['7.7', '8.5', '6.4', '7.9', '4.5', '5.5', '9.9'];
 
 // sem callback
-let notasBaixas = []
+let notasBaixas1 = []
 for(let i in notas){
     if(notas[i] < 7){
-        notasBaixas.push(notas[i])
+        notasBaixas1.push(notas[i])
     }
 }
+
+console.log(notasBaixas1)
+
+//com callback
+const notasBaixas2 = notas.filter(function(nota){
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notas.filter(notasMenorQue7)
+console.log(notasBaixas3)
