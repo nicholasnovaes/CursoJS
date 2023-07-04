ac2082c085c464ad1ddf05f22687afb6bd363b2f
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-07-03 21:15:39 -0300

    Objeto

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+//codigo nao executavel!!
+
+//procedural
+processamento(valor1, valor2, valor3 )
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+obejto.processamento() //foco passou a ser o objeto
+
+/* Principios importantes:
+1. abstração
+2.encapsulamento
+3.herança(prototype)
+4,polimorfismo
+*/
